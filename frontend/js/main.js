410f11baaee5d403bd04e963b089c4351aa43109
SciBridge <scibridgelabs@gmail.com>
2024-07-05 22:19:35 +0530

    add cicd

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 const counter = document.querySelector("#counter");
 const updateCounter = async () => {
   let response = await fetch(
-    "https://lsr5o42aqlbfhj6lxcfmryb6pu0qkcdm.lambda-url.ap-south-1.on.aws/"
+    "https://ln2sa2gdyjj4j4wokc4xuotguy0hydbr.lambda-url.ap-south-1.on.aws/"
   );
   let data = await response.json();
   counter.innerText = `${data}`;
